51d84ccbaf8c153dd1f6b1646da81550541107f3
Earl Fong <earl_fong@operamail.com>
2020-06-20 18:56:20 -0700

    Add a bit of error checking

--- sensors.py
+++ sensors.py
@@ -4,26 +4,29 @@
 import subprocess
 import re
 import datetime
+import sys
 
 cmd = subprocess.Popen(["sensors"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 stdout, stderr = cmd.communicate()
 
+out = err = ""
 if stderr is None:
     out = stdout.decode("utf-8")
 else:
     err = stderr.decode("utf-8")
 
-re_result = re.findall(r"(.*\+[8-9]\d\.\d).C", out)
+if out:
+    re_result = re.findall(r"(.*\+[4-9]\d\.\d).C", out)
+    now = str(datetime.datetime.now())[:-10]  # Truncate the seconds and fractional seconds
+    if re_result:
+        # N.B. If this code is run by cron as user earl, the sensors.out file will be in /home/earl
+        with open("sensors.out", "a") as fp:
+            fp.write("\n========= " + now + "\n")
+            for res in re_result:
+                fp.write(res + "\n")
 
-now = str(datetime.datetime.now())[:-10]  # Truncate the seconds and fractional seconds
-
-print(now)
-for res in re_result:
-    print(res)
-
-if re_result:
-    # N.B. If this code is run by cron as user earl, the sensors.out file will be in /home/earl
+elif err:
     with open("sensors.out", "a") as fp:
         fp.write("\n========= " + now + "\n")
-        for res in re_result:
-            fp.write(res + "\n")
+        fp.write("The following error occurred while invoking 'sensors'\n")
+        fp.write(err)
